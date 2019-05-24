a128146dd4773144ca29047dd77c202331bceb38
Shou Ya <shouya@users.noreply.github.com>
2019-05-24 21:30:56 +0800

    day one hanoi tower

---
+++ 001-hanoi-tower/shou.hs
@@ -0,0 +1,22 @@
+hanoi :: Int -> [(Int, Int)]
+hanoi n = hanoi' n 1 3 2
+
+hanoi' :: Int -> Int -> Int -> Int -> [(Int, Int)]
+hanoi' 0 _ _ _ = []
+hanoi' n a b c = (hanoi' (n - 1) a c b) ++
+                 [(a, b)] ++
+                 (hanoi' (n - 1) b c a)
+
+
+printSteps :: [(Int, Int)] -> IO ()
+printSteps [] = return ()
+printSteps ((a,b):xs) = do
+  print (a,b)
+  printSteps xs
+
+main :: IO ()
+main = let n = 3
+           steps = hanoi 3
+       in printSteps steps
+
+
