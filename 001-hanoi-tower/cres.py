b7d313d020ab4850010843e9ca45d150f1d77715
crescat <crescentiacat@gmail.com>
2019-05-24 22:25:31 +0800

    day 1 hanoi tower (#2)
    * day 1
    
    * added new line

--- 001-hanoi-tower/cres.py
+++ 001-hanoi-tower/cres.py
@@ -1,6 +1,30 @@
 # Day 1: Hanoi Tower
-def hanoi(n, left='left', middle='middle', right='right'):
+def hanoi(n, source = 1, target = 3, spare = 2):
+    if n < 0:
+        return 'Invalid input!'
+    
+    if n == 0:
+        return []
+
     if n > 0:
-        hanoi(n-1, left, right, middle)
-        print(left, 'to', right)
-        hanoi(n-1, middle, left, right)
+        return \
+            hanoi(n - 1, source, spare, target) + \
+            [move(source, target)] + \
+            hanoi(n - 1, spare, target, source)
+        
+
+def move(a, b):
+    return (a, b)
+
+
+def check_hanoi(n, sequence):
+    board = [n, 0, 0]
+    for a, b in sequence:
+        board[a-1] -= 1
+        board[b-1] += 1
+    return board
+
+n = 5
+
+print(hanoi(n))
+print(check_hanoi(n, hanoi(n)))
