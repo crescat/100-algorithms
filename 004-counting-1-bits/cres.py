ca53f7800ebde40e20f847126838eb065ce39fba
crescat <crescentiacat@gmail.com>
2019-05-26 00:27:30 +0900

    Day 4 counting 1 bits (#5)
    * day 4 counting 1 bits
    
    * add new line at the end
    
    * try other test case

---
+++ 004-counting-1-bits/cres.py
@@ -0,0 +1,9 @@
+def counting_1_bits(binary_number):
+    count = 0
+    while binary_number > 0:
+        binary_number &= binary_number - 1
+        count += 1
+    return count
+
+
+print(counting_1_bits(0b1010010110101111101))
