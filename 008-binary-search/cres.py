240a6bf10cb4b158b37b1d0808ebd8da2c4e14dd
cres <crescat@users.noreply.github.com>
2019-05-26 17:56:32 +0800

    add base case and handle non-existing items

--- 008-binary-search/cres.py
+++ 008-binary-search/cres.py
@@ -1,6 +1,9 @@
 # Day 8: Binary search on sorted array
 
 def search(array, item, cursor):
+    if array == []:
+        return False
+
     if len(array) == 1:
         if array[0] == item:
             return cursor
@@ -16,4 +19,7 @@ def search(array, item, cursor):
     elif item >= right[0]:
         return search(right, item, cursor+middle)
 
-print(search([1,2,3,4,6,7,9,10,11], 9, 0))
+    else:
+        return False
+
+print(search([1,2,3,4,6,7,9,10,11], 8, 0))
