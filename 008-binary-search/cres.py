80c50344e182028cd356d81b0aa8cdab5b444c1b
cres <crescat@users.noreply.github.com>
2019-05-26 17:53:14 +0800

    make cursor properly work

--- 008-binary-search/cres.py
+++ 008-binary-search/cres.py
@@ -9,7 +9,11 @@ def search(array, item, cursor):
     middle = len(array) // 2
     left = array[:middle]
     right = array[middle:]
-    if item < left[-1]:
-        return search(left, item, middle-1)
+
+    if item <= left[-1]:
+        return search(left, item, cursor)
+
     elif item >= right[0]:
-        return search(right, item, middle)
+        return search(right, item, cursor+middle)
+
+print(search([1,2,3,4,6,7,9,10,11], 9, 0))
