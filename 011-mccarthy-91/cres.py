9b6968d5e0f721dc2ff5b96d1892c04d0fdd9160
cres <crescat@users.noreply.github.com>
2019-05-28 15:20:30 +0800

    trying different numbers for McCarthy function

--- 011-mccarthy-91/cres.py
+++ 011-mccarthy-91/cres.py
@@ -7,3 +7,12 @@ def mccarthy(n):
         return mccarthy(mccarthy(n + 11))
 
 print(mccarthy(6))
+
+def n_mccarthy(n, x):
+    if n > x:
+        return n - 10
+    else:
+        return n_mccarthy(n_mccarthy(n + 11, x), x)
+
+for x in range(1, 100):
+    print(n_mccarthy(50, x))
