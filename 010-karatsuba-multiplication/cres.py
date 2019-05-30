bd47109db7a49141e1295599658dd739f0de34a8
cres <crescat@users.noreply.github.com>
2019-05-30 21:07:16 +0800

    added naive multiplication and test case

--- 010-karatsuba-multiplication/cres.py
+++ 010-karatsuba-multiplication/cres.py
@@ -1,3 +1,5 @@
+import random
+
 def karatsuba_mult(a, b):
     binary_a = bin(a)[::-1]
     result = 0
@@ -6,5 +8,24 @@ def karatsuba_mult(a, b):
             result += b * 2 ** i
     return result
 
-print(karatsuba_mult(75431456753245638765348576526448776785674432343578843623, \
-    243562124645233647544337879708765452634856756467657542324467543214))
+
+def naive_mult(a, b):
+    x, y = str(a), str(b)
+    result = 0
+
+    lst = [[int(i) * int(j) for j in y[::-1]] for i in x[::-1]]
+
+    for m in range(len(lst)):
+        for n in range(len(lst[0])):
+            result += lst[m][n] * 10 ** (m + n)
+
+    return result
+
+N = 500
+A = int(''.join([str(random.randint(0, 9)) for _ in range(N)]))
+B = int(''.join([str(random.randint(0, 9)) for _ in range(N)]))
+
+import cProfile
+cProfile.run('karatsuba_mult(A, B)')
+cProfile.run('naive_mult(A, B)')
+cProfile.run('A * B')
